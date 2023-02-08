92c0d74b1b2e969d5ad1ae4f68a96b04a557bb17
sococa <pierrocarlier@gmail.com>
2023-02-08 15:10:08 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercice_carte_front';
+  title = 'Jeu de cartes LevelUp';
 }
